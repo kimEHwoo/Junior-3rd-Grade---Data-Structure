9d4494f43b9ae308ccc53e58cb103d7ad3378e28
김태우 <kimtaewoo721@gmail.com>
2024-03-15 15:24:23 +0900

    Boolean function

--- Main.py
+++ Main.py
@@ -1,3 +1,14 @@
-price = 12
-price = '{:.3f}'.format(price)
-print(price)
+x = 5
+y = 10
+is_greater = x > y
+is_equal = x == y
+
+print(is_greater)
+print(is_equal)
+
+is_raining = True
+is_cold = False
+is_wet_and_cold = is_raining and is_cold
+print(is_wet_and_cold)
+is_raining_or_cold = is_raining or is_cold
+print(is_raining_or_cold)
