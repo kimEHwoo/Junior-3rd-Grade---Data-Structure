c4d45e701dda51fb00c77162610bbb418461276b
김태우 <kimtaewoo721@gmail.com>
2024-03-15 15:21:01 +0900

    float decimal point

--- Main.py
+++ Main.py
@@ -1,8 +1,3 @@
-years = input("Please enter number of years: ")
-months = input("Please enter number of months: ")
-
-days = int(years) * 365 + int(months) * 30
-
-result = years + " years and " + months + " months are equal with " + str(days) + " days"
-
-print(result)
+price = 12
+price = '{:.3f}'.format(price)
+print(price)
