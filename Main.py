529ba66c603aefe04cbb3393baaf3046049b571e
김태우 <kimtaewoo721@gmail.com>
2024-03-15 15:28:18 +0900

    format()

--- Main.py
+++ Main.py
@@ -1,14 +1,4 @@
-x = 5
-y = 10
-is_greater = x > y
-is_equal = x == y
+followers = 47
+print(type(followers))
 
-print(is_greater)
-print(is_equal)
-
-is_raining = True
-is_cold = False
-is_wet_and_cold = is_raining and is_cold
-print(is_wet_and_cold)
-is_raining_or_cold = is_raining or is_cold
-print(is_raining_or_cold)
+print('You have {} followers!'.format(followers))
