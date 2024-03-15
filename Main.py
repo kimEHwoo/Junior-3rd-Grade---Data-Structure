d7e06bef43b3fe27fd632ed4110aa8d4424adafc
김태우 <kimtaewoo721@gmail.com>
2024-03-15 15:47:17 +0900

    list Indexing

--- Main.py
+++ Main.py
@@ -1,19 +1,7 @@
-my_list = [3,1,2,4,5]
+tempInMiami = [27, 29, 30, 30, 28, 29, 30]
 
-my_list.sort()
-print(my_list)
+firstItem = tempInMiami[0]
+print('The first item is {}'.format(firstItem))
 
-my_list.reverse()
-print(my_list)
-
-my_list.append(6)
-print(my_list)
-
-my_list.insert(2, 10)
-print(my_list)
-
-my_list.remove(4)
-print(my_list)
-
-popped_element = my_list.pop()
-print(my_list)
+minTemp = min(tempInMiami)
+print('The minimum temperature is {}'.format(minTemp))
