ccb95192063a44200256b6d98050dd58719a5737
김태우 <kimtaewoo721@gmail.com>
2024-03-15 15:44:20 +0900

    list function

--- Main.py
+++ Main.py
@@ -1,18 +1,19 @@
-my_list = [1,2,3,4,5]
-print(my_list[1:4])
-print(my_list[::-1])
+my_list = [3,1,2,4,5]
 
-my_list.append(6)
-my_list.extend([7,8])
-del my_list[1]
-
+my_list.sort()
 print(my_list)
 
-list1 = [1,2,3]
-list2 = [4,5,6]
-concatenated_list = list1 + list2
-print(concatenated_list)
-repeated_list = list1 * 3
-print(repeated_list)
-print(2 in list1)
-print(len(list1))
+my_list.reverse()
+print(my_list)
+
+my_list.append(6)
+print(my_list)
+
+my_list.insert(2, 10)
+print(my_list)
+
+my_list.remove(4)
+print(my_list)
+
+popped_element = my_list.pop()
+print(my_list)
